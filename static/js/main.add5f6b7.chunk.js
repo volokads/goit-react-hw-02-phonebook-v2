24c5c5551ce8d98aab0f7648a5074837b7ab1b4b
volokads <hluhovspg@gmail.com>
2021-11-29 22:44:01 +0200

    Updates

--- static/js/main.add5f6b7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook-v2"]=this["webpackJsonpgoit-react-hw-02-phonebook-v2"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(9),n(0));var a=function(){return Object(s.jsx)("div",{className:"App"})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.add5f6b7.chunk.js.map
